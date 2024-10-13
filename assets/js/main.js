52b1099edee8571cd965c500d02a0bd066120f34
TrEKi722 <105758761+TrEKi722@users.noreply.github.com>
2024-10-12 22:42:50 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -408,16 +408,17 @@ showSlides(1, 0);
 showSlides(1, 1);
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+	showSlides(slideIndex[no] += n, no);
 }
 
 function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
+	let i;
+	let x = document.getElementsByClassName(slideId[no]);
+	if (n > x.length) {slideIndex[no] = 1}
+	if (n < 1) {slideIndex[no] = x.length}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex[no]-1].style.display = "block";
+	setTimeout(showSlides, 10000);  // Change image every 10 seconds
 }
