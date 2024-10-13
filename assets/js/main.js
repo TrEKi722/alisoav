f22e351a1c688ebc0ff613c1aca0b418f450f6f1
TrEKi722 <105758761+TrEKi722@users.noreply.github.com>
2024-10-12 22:38:41 -0700

    update slideshow js

--- assets/js/main.js
+++ assets/js/main.js
@@ -400,18 +400,24 @@
 
 })(jQuery);
 
-// Slideshow JS
-let slideIndex = 0;
-showSlides();
+// Slideshows
+let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function showSlides() {
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
+  x[slideIndex[no]-1].style.display = "block";
 }
